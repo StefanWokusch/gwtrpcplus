76a804c9d22b3bdca475b8b73438c2aa2ff6330c
Stefan Wokusch <stefan@wokusch.de>
2014-04-24 18:16:34 +0200

    Fixed Keepalive for Websockets

--- gwtRpcPlus/server/src/main/java/com/googlecode/gwtrpcplus/server/internal/RpcPlusClient.java
+++ gwtRpcPlus/server/src/main/java/com/googlecode/gwtrpcplus/server/internal/RpcPlusClient.java
@@ -14,122 +14,124 @@ import com.googlecode.gwtrpcplus.server.internal.util.Logger;
  * A connected Client to the Server
  */
 public class RpcPlusClient {
-	/**
-	 * Milliseconds when the client isn't sending anything, the Client will be marked as obsolete
-	 */
-	private static final long OBSOLETE_TIMEOUT = TimeUnit.MINUTES.toMillis(1);
+  /**
+   * Milliseconds when the client isn't sending anything, the Client will be marked as obsolete
+   */
+  private static final long OBSOLETE_TIMEOUT = TimeUnit.MINUTES.toMillis(1);
 
-	/**
-	 * WARN: have to equals TimeoutTimer.TIMEOUT
-	 */
-	static final long KEEPALIVE_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
+  /**
+   * WARN: have to equals TimeoutTimer.TIMEOUT
+   */
+  static final long KEEPALIVE_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
 
-	private Logger logger = new Logger(RpcPlusClient.class);
+  private Logger logger = new Logger(RpcPlusClient.class);
 
-	private final Set<RpcPlusClientHandler> handlers = Collections.newSetFromMap(new ConcurrentHashMap<RpcPlusClientHandler, Boolean>());
+  private final Set<RpcPlusClientHandler> handlers = Collections.newSetFromMap(new ConcurrentHashMap<RpcPlusClientHandler, Boolean>());
 
-	private final BlockingQueue<String> responses = new LinkedBlockingQueue<String>();
+  private final BlockingQueue<String> responses = new LinkedBlockingQueue<String>();
 
-	private boolean disconnected = false;
+  private boolean disconnected = false;
 
-	private long lastCall = System.currentTimeMillis();
-	private long lastAnswer = System.currentTimeMillis();
+  private long lastCall = System.currentTimeMillis();
+  private long lastAnswer = System.currentTimeMillis();
 
-	/**
-	 * Handler for Answers
-	 */
-	public interface RpcPlusClientHandler {
-		/**
-		 * Called when a answer was created for a client
-		 * 
-		 * @param answer
-		 *          the Answer to send
-		 * @return true when the answer could be send, false for example on ws-disconnect
-		 */
-		boolean onAnswer(String answer);
-	}
+  /**
+   * Handler for Answers
+   */
+  public interface RpcPlusClientHandler {
+    /**
+     * Called when a answer was created for a client
+     * 
+     * @param answer the Answer to send
+     * @return true when the answer could be send, false for example on ws-disconnect
+     */
+    boolean onAnswer(String answer);
+  }
 
-	public HandlerRegistration addHandler(final RpcPlusClientHandler handler) {
-		handlers.add(handler);
-		return new HandlerRegistration() {
-			@Override
-			public void removeHandler() {
-				handlers.remove(handler);
-			}
-		};
-	}
+  public HandlerRegistration addHandler(final RpcPlusClientHandler handler) {
+    handlers.add(handler);
+    return new HandlerRegistration() {
+      @Override
+      public void removeHandler() {
+        handlers.remove(handler);
+      }
+    };
+  }
 
-	/**
-	 * @return true when the Client isn't called the last time and can be cleaned
-	 */
-	public boolean isObsolete() {
-		// TODO Remove this check
-		// Don't remove when an active websocketconnection is established
-		if (!handlers.isEmpty())
-			return false;
+  /**
+   * @return true when the Client isn't called the last time and can be cleaned
+   */
+  public boolean isObsolete() {
+    // TODO Remove this check
+    // Don't remove when an active websocketconnection is established
+    if (!handlers.isEmpty())
+      return false;
 
-		return disconnected || (System.currentTimeMillis() - lastCall) > OBSOLETE_TIMEOUT;
-	}
+    return disconnected || (System.currentTimeMillis() - lastCall) > OBSOLETE_TIMEOUT;
+  }
 
-	/**
-	 * Mark the Client as inUse
-	 */
-	public void touch() {
-		lastCall = System.currentTimeMillis();
-	}
+  /**
+   * Mark the Client as inUse
+   */
+  public void touch() {
+    lastCall = System.currentTimeMillis();
+  }
 
-	public void addResponse(String response) {
-		if (disconnected) {
-			logger.trace("Ignoring Response because of client disconnected");
-			return;
-		}
-		for (RpcPlusClientHandler h : handlers) {
-			if (h.onAnswer(response)) {
-				touch();
-				return;
-			}
-		}
+  public void addResponse(String response) {
+    if (disconnected) {
+      logger.trace("Ignoring Response because of client disconnected");
+      return;
+    }
+    for (RpcPlusClientHandler h : handlers) {
+      if (h.onAnswer(response)) {
+        touch();
+        return;
+      }
+    }
 
-		responses.add(response);
-	}
+    responses.add(response);
+  }
 
-	public String getResponse() {
-		touch();
-		lastAnswer = System.currentTimeMillis();
-		return responses.poll();
-	}
+  public String getResponse() {
+    touch();
+    String response = responses.poll();
+    if (response != null)
+      lastAnswer = System.currentTimeMillis();
+    return response;
+  }
 
-	public String getResponseAndWait() {
-		try {
-			// Timeout will be calculated by the maximum keepalive-time. After that it will subtract
-			// something to be sure, the client will recieve it in time.
-			long timeout = Math.min(KEEPALIVE_TIMEOUT, KEEPALIVE_TIMEOUT - (System.currentTimeMillis() - lastAnswer)) - 1000;
-			logger.trace("waiting {} ms for responses", timeout);
-			return responses.poll(timeout, TimeUnit.MILLISECONDS);
-		} catch (InterruptedException e) {
-			return null;
-		} finally {
-			touch();
-			lastAnswer = System.currentTimeMillis();
-		}
-	}
+  public String getResponseAndWait() {
+    try {
+      // Timeout will be calculated by the maximum keepalive-time. After that it will subtract
+      // something to be sure, the client will recieve it in time.
+      long timeout = Math.min(KEEPALIVE_TIMEOUT, KEEPALIVE_TIMEOUT - (System.currentTimeMillis() - lastAnswer)) - 1000;
+      logger.trace("waiting {} ms for responses", timeout);
+      return responses.poll(timeout, TimeUnit.MILLISECONDS);
+    } catch (InterruptedException e) {
+      return null;
+    } finally {
+      touch();
+      lastAnswer = System.currentTimeMillis();
+    }
+  }
 
-	public void disconnect() {
-		if (!disconnected) {
-			disconnected = true;
-			responses.clear();
+  public void disconnect() {
+    if (!disconnected) {
+      disconnected = true;
+      responses.clear();
 
-			// TODO Inform Serverpush-Methods
+      // TODO Inform Serverpush-Methods
 
-			// TODO Inform Handlers
-		}
-	}
+      // TODO Inform Handlers
+    }
+  }
 
-	public void keepAlive() {
-		if (!isObsolete() && !handlers.isEmpty() && System.currentTimeMillis() - lastAnswer > KEEPALIVE_TIMEOUT) {
-			logger.info("Sending keepalive");
-			addResponse(".");
-		}
-	}
+  public void keepAlive() {
+    long timeSinceLastAnswer = System.currentTimeMillis() - lastAnswer;
+    if (!isObsolete() && !handlers.isEmpty() && timeSinceLastAnswer > KEEPALIVE_TIMEOUT / 2) {
+      logger.info("Sending keepalive");
+      addResponse(".");
+    }
+  }
 
 }
